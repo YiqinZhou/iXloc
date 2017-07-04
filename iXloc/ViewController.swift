811f8151352f22d7138b8d12480aaea72f7f60d2
Yiqin Zhou <yiqin.zhou@duke.edu>
2017-07-04 15:34:32 +0200

    Initial Commit

---
+++ iXloc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iXloc
+//
+//  Created by ZhouYiqin on 7/4/17.
+//  Copyright © 2017 Yiqin Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
