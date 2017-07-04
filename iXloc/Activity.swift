f215d73590d00d7e0acc5eaabdecfd87d543cec4
Yiqin Zhou <yiqin.zhou@duke.edu>
2017-07-04 16:24:12 +0200

    Add activity array

---
+++ iXloc/Activity.swift
@@ -0,0 +1,19 @@
+//
+//  Activity.swift
+//  iXloc
+//
+//  Created by ZhouYiqin on 7/4/17.
+//  Copyright © 2017 Yiqin Zhou. All rights reserved.
+//
+
+import Foundation
+
+class Activity{
+    var name: String?
+    var description: String?
+    
+    init(name: String?, description: String?){
+        self.name=name
+        self.description=description
+    }
+}
