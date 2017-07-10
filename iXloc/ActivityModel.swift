442f28dab8ac0d43eeedde7ba833cdc4504da3a0
Yiqin Zhou <yiqin.zhou@duke.edu>
2017-07-10 16:45:07 +0200

    Add Realm

---
+++ iXloc/ActivityModel.swift
@@ -0,0 +1,16 @@
+//
+//  ActivityModel.swift
+//  iXloc
+//
+//  Created by ZhouYiqin on 7/10/17.
+//  Copyright © 2017 Yiqin Zhou. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class ActivityModel: Object{
+    dynamic var name = ""
+    dynamic var descrp = ""
+    
+}
